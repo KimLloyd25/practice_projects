6c90e76b1ea4b0520984fc46a47cdc49fc59509a
KimLloyd25 <51363969+KimLloyd25@users.noreply.github.com>
2025-07-21 08:46:43 +0100

    Update rock_paper_scissors.py
    Updated code to move the computer variable that chooses a random option inside the while loop as it was accidentally placed outside of the loop and was not updating when a new game started.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -7,32 +7,34 @@ from random import randint
 play = "Y"
 
 #Set the options variable to collection of options
-options = ["Rock", "Paper", "Scissors"]
-
-#Randomly select a value from options for the computer
-computer = options[randint(0,2)]
+options = ["rock", "paper", "scissors"]
 
 #Loop through game
 while (play == "Y" or "y"):
 
+  #Randomly select a value from options for the computer
+  computer = options[randint(0,2)]
+  print(computer)
+
   #Ask player for their choice of rock, paper or scissors
   player = input("Rock, paper, scissors? ")
+  player = player.lower()
 
   #If statements to decide winner of game and output messages
   if player == computer:
     print("It's a tie!")
-  elif player == "Rock":
-    if computer == "Paper":
+  elif player == "rock":
+    if computer == "paper":
       print(f"You lose! {computer} covers {player}")
     else:
       print(f"You win! {player} smashes {computer}")
-  elif player == "Paper":
-    if computer == "Rock":
+  elif player == "paper":
+    if computer == "rock":
       print(f"You win! {player} covers {computer}")
     else:
       print(f"You lose! {computer} cuts {player}")
-  elif player == "Scissors":
-    if computer == "Rock":
+  elif player == "scissors":
+    if computer == "rock":
       print(f"You win! {player} smashes {computer}")
     else:
       print(f"You lose! {computer} cuts {player}")
@@ -45,8 +47,10 @@ while (play == "Y" or "y"):
   #Ask player if they want to play again
   play = input("Would you like to play again? (Y/N) ")
   if play == "Y":
+    computer = ""
     continue
   elif play == "y":
+    computer = ""
     continue
   else:
     print("GAME OVER")
